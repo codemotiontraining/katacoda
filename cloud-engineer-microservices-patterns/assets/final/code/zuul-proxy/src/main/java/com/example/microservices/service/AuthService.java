4a7d37e72431904b621facc7e9ddddd47555cc8a
codemotiontraining <training@codemotion.it>
2021-03-06 15:44:08 +0100

    fixed bearer in Code

--- cloud-engineer-microservices-patterns/assets/final/code/zuul-proxy/src/main/java/com/example/microservices/service/AuthService.java
+++ cloud-engineer-microservices-patterns/assets/final/code/zuul-proxy/src/main/java/com/example/microservices/service/AuthService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 public class AuthService {
 
     /* ~~~ OVVIAMENTE NON METTERE QUESTI VALORI QUI IN UN CASO REALE! ~~~~ */
-    private final static String TOKEN = "bearer Fr45dgUDJs8e3hdjke3idhj3hdk8hd";
+    private final static String TOKEN = "Bearer Fr45dgUDJs8e3hdjke3idhj3hdk8hd";
     private final static String ADMIN = "shopAdmin";
     private final static String PASSWORD = "secretPassword";
     /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
