483a54247e50019cc8868e50b6c267ea2891f4f4
Alessandro Argentieri <alexmawashi87@gmail.com>
2022-03-25 17:18:37 +0100

    Update prepare.sh

--- cloud-engineer-cloud-native/assets/prepare.sh
+++ cloud-engineer-cloud-native/assets/prepare.sh
@@ -2,6 +2,8 @@
 
 echo "Preparing the environment..."
 
+export TAG='v4.4.0'
 curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
 snap install kubectl --classic
 source ~/.bashrc
+export PATH=$PATH:/root/katacoda/cloud-engineer-cloud-native/assets
